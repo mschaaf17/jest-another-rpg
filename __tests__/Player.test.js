4044bd968ddac5a4e553edd4af0197c789f024ad
Maddy <madisonschaaf48@gmail.com>
2022-03-19 15:39:26 -0600

    ran tests for properties of player

---
+++ __tests__/Player.test.js
@@ -0,0 +1,36 @@
+const Player = require('../lib/Player.js')
+
+jest.mock('../lib/Potion.js')
+console.log(new Potion());
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+ 
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number))
+    expect(player.strength).toEqual(expect.any(Number))
+    expect(player.agility).toEqual(expect.any(Number))
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    )
+})
+
+test('gets players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+})
+
+test('gets inventory from player or retruns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(array));
+
+    player.inventory = []
+
+    expect(player.getInventory()).toEqual(false)
+})
